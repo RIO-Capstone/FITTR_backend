e85397872bc68c8eed5fc1cc67b5b6074980381a
AnshOz <ansh_oswal@mymail.sutd.edu.sg>
2025-03-06 12:58:26 +0800

    Merged User Page

--- FITTR_API/FITTR_API/urls.py
+++ FITTR_API/FITTR_API/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-from .db_utils.user_utils import register_user, get_all_users, get_user, login_user, get_user_history
+from .db_utils.user_utils import register_user, get_all_users, get_user, login_user, get_user_history, get_users_by_product
 from .db_utils.product_utils import register_product, get_all_products, get_product
 from .ai_utils.ai_assistant import get_ai_feedback, get_feedback_on_latest_exercise_session, get_ai_rep_generation
 
@@ -21,7 +21,8 @@ user_paths = [
     path('user/<int:id>/history',get_user_history),
     path('user/latest_exercise_session_feedback',get_feedback_on_latest_exercise_session),
     path('user/<int:user_id>/ai_rep_generation', get_ai_rep_generation),
-    path('user/<int:user_id>/ai_feedback', get_ai_feedback)
+    path('user/<int:user_id>/ai_feedback', get_ai_feedback),
+    path('user/product/<int:product_id>', get_users_by_product)
 ]
 
 product_paths = [
